f2ca08ea98b45661495ca6e6df43b24db0aa93dd
Michelle Blevins <mjblevins7@gmail.com>
2018-03-18 23:11:02 +0000

    automatically backed up by learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,19 @@
+def instantiate_new_array
+  new_array = Array.new
+end
+
+def array_with_two_elements
+  my_array = ["one", "two"]
+end
+
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[-1]
+end
